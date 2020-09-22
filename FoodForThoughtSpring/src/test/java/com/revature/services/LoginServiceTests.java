303cf5e1a66a86bb9fa8c870a8183c04a5a4312e
Nicole Lang <nicole.lang@revature.net>
2020-09-21 21:29:37 -0400

    attempted Mockito; added logging
    -tried to set up mock tests but wasn't able to get it working
    -added log4j2 logging (feel free to change the path in the properties file if you can't find the log)
    -cleaned up some dead code

--- FoodForThoughtSpring/src/test/java/com/revature/services/LoginServiceTests.java
+++ FoodForThoughtSpring/src/test/java/com/revature/services/LoginServiceTests.java
@@ -1,40 +1,52 @@
-//package com.revature.services;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//import org.junit.jupiter.api.BeforeAll;
-//import org.junit.jupiter.api.Test;
-//
-//import com.revature.models.User;
-//import com.revature.repositories.IUserDAO;
-//
-//class LoginServiceTests {
-//
-//	public static LoginService ls;
-//	public static IUserDAO uDao;
-//	public static User u;
-//	
-//	@BeforeAll
-//	static void setUpBeforeClass() throws Exception {
-//		ls = new LoginService(uDao);
-//		u = new User();
-//		u.setUsername("Olivia");
-//		u.setPassword("5B5921AA233DB59A0499718B5E69A165");
-//	}
-//
-//	@Test
-//	void testLogin() {
-//		System.out.println("Testing login");
-//		assertTrue(ls.login(u));
-//		System.out.println("Login test successful");
-//		
-//	}
-//	@Test
-//	void testFindUser() {
-//		System.out.println("Testing find by username");
-//		User f = (ls.findUser(u.getUsername()));
-//		assertEquals(f.getId(), 1);
-//		System.out.println("Find user test successful");
-//	}
-//
-//}
+//still doesn't work, can't figure it out
+
+package com.revature.services;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ContextConfiguration;
+
+import com.revature.models.User;
+import com.revature.repositories.IUserDAO;
+@ContextConfiguration
+@DataJpaTest
+class LoginServiceTests {
+
+	@MockBean
+	public static IUserDAO uDao;
+	@Autowired
+	public static LoginService ls;
+	public static User u;
+	
+	@BeforeAll
+	static void setUp(){
+		u = new User();
+		u.setId(1);
+		u.setUsername("test");
+		u.setPassword("test");
+		Mockito.when(uDao.findByUsername("test"))
+	      .thenReturn(u);
+	}
+
+	@Test
+	void testLogin() {
+		System.out.println("Testing login");
+		assertTrue(ls.login(u));
+		System.out.println("Login test successful");
+		
+	}
+	@Test
+	void testFindUser() {
+		System.out.println("Testing find by username");
+		User f = (ls.findUser(u.getUsername()));
+		assertEquals(f.getId(), 1);
+		System.out.println("Find user test successful");
+	}
+
+}
