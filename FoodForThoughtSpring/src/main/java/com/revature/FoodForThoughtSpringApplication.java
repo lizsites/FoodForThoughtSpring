b19d5b068a23ef4ec8fdf43d5704f08f9d2586a2
lizsites <69205363+lizsites@users.noreply.github.com>
2020-09-24 17:13:23 -0500

    clarifying scope

--- FoodForThoughtSpring/src/main/java/com/revature/FoodForThoughtSpringApplication.java
+++ FoodForThoughtSpring/src/main/java/com/revature/FoodForThoughtSpringApplication.java
@@ -1,29 +1,16 @@
 package com.revature;
 
-//import org.junit.runner.Description;
-//import org.junit.runner.Runner;
-//import org.junit.runner.notification.RunNotifier;
+
 import org.springframework.boot.SpringApplication;  
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FoodForThoughtSpringApplication /*extends Runner*/ {
+public class FoodForThoughtSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FoodForThoughtSpringApplication.class, args);
 		
 	}
 
-//	@Override
-//	public Description getDescription() {
-//		// TODO Auto-generated method stub
-//		return null;
-//	}
-//
-//	@Override
-//	public void run(RunNotifier notifier) {
-//		// TODO Auto-generated method stub
-//		
-//	}
 
 }
