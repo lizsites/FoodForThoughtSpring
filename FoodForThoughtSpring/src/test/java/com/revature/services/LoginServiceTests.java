b4f305fde3e7e46f10b39b6b4cd5c16e56ad36d7
lizsites <69205363+lizsites@users.noreply.github.com>
2020-09-24 09:56:49 -0500

    working on tests but still commented out

--- FoodForThoughtSpring/src/test/java/com/revature/services/LoginServiceTests.java
+++ FoodForThoughtSpring/src/test/java/com/revature/services/LoginServiceTests.java
@@ -4,38 +4,91 @@
 //
 //import static org.junit.jupiter.api.Assertions.*;
 //
+//import java.util.ArrayList;
+//import java.util.Optional;
+//
+//import org.junit.Before;
+//import org.junit.jupiter.api.AfterAll;
 //import org.junit.jupiter.api.BeforeAll;
 //import org.junit.jupiter.api.Test;
-//import org.mockito.Mockito;
+//import org.junit.runner.RunWith;
+//import org.mockito.InjectMocks;
+//import org.mockito.Mock;
+//import org.mockito.MockitoAnnotations;
 //import org.springframework.beans.factory.annotation.Autowired;
 //import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+//import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+//import org.springframework.boot.test.context.SpringBootTest;
 //import org.springframework.boot.test.mock.mockito.MockBean;
-//import org.springframework.test.context.ContextConfiguration;
 //
+//import com.revature.FoodForThoughtSpringApplication;
+//import com.revature.controllers.LoginController;
+//import com.revature.controllers.RecipeController;
+//import com.revature.models.Picture;
+//import com.revature.models.Preferences;
+//import com.revature.models.Preferences.DietType;
+//import com.revature.models.Recipe;
 //import com.revature.models.User;
+//import com.revature.repositories.IRecipeDAO;
 //import com.revature.repositories.IUserDAO;
-//@ContextConfiguration
-//@DataJpaTest
+//import com.revature.repositories.IngredientDAO;
+//
+//@RunWith(FoodForThoughtSpringApplication.class)
+//@SpringBootTest
 //class LoginServiceTests {
 //
-//	@MockBean
-//	public static IUserDAO uDao;
-//	@Autowired
+//	
 //	public static LoginService ls;
 //	public static User u;
+//	public static PictureService ps;
+//	public static TestEntityManager entityManager;
 //	
-//	@BeforeAll
-//	static void setUp(){
-//		u = new User();
-//		u.setId(1);
-//		u.setUsername("test");
-//		u.setPassword("test");
-//		Mockito.when(uDao.findByUsername("test"))
-//	      .thenReturn(u);
+//	@InjectMocks
+//	private LoginController lc;
+//	
+//	@InjectMocks
+//	private RecipeController rc;
+//	
+//	@Mock
+//	public static IUserDAO userDAO;
+//	
+//	@Mock
+//	public static IRecipeDAO recipeDao;
+//	
+//	@Mock
+//	public static IngredientDAO ingredientDAO;
+//	
+//	
+//	@Autowired(required = true)
+//	public LoginServiceTests(LoginService ls, User u, PictureService ps, IUserDAO userDAO, TestEntityManager entityManager) {
+//		this.ls = ls;
+//		this.u = u;
+//		this.ps = ps;
+//		this.userDAO = userDAO;
+//		this.entityManager = entityManager;
 //	}
+//	
+//	@Before
+//	public void init() {
+//		MockitoAnnotations.initMocks(this);
+//	}
+//	
+////	@BeforeAll
+////	static void setUp(){
+////		
+////	}
 //
 //	@Test
 //	void testLogin() {
+//		u = new User();
+//		u.setUsername("test");
+//		u.setPassword("test");
+//		u.setRecipes(new ArrayList<Recipe>());
+//		u.setPictures(new ArrayList<Picture>());
+//		u.setPreference(new Preferences(DietType.GLUTEN_FREE,0,0));
+//		System.out.println("u right before being tested" + u);
+//		entityManager.persist(u);
+//		u.setId(ls.findUser(u.getUsername()).getId());
 //		System.out.println("Testing login");
 //		assertTrue(ls.login(u));
 //		System.out.println("Login test successful");
@@ -45,8 +98,22 @@
 //	void testFindUser() {
 //		System.out.println("Testing find by username");
 //		User f = (ls.findUser(u.getUsername()));
-//		assertEquals(f.getId(), 1);
+//		assertEquals(f.getId(), u.getId());
 //		System.out.println("Find user test successful");
+//	}
+//	
+//	@Test
+//	void testAddPicture() {
+//		System.out.println("Test addPicture()");
+//		Picture pic = new Picture();
+//		ps.savePicture(u, pic);
+//		assertTrue(ls.findUser(u.getUsername()).getPictures().size() > 0);
+//	}
+//	
+//	
+//	@AfterAll
+//	static void tearDown() {
+//		userDAO.delete(u);
 //	}
 //
 //}
