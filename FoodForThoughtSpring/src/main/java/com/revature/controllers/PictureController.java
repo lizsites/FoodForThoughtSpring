45b49edaba819b31a7e7ac11800ce5a8cb63f59f
lizsites <69205363+lizsites@users.noreply.github.com>
2020-09-24 17:49:42 -0500

    making request body

--- FoodForThoughtSpring/src/main/java/com/revature/controllers/PictureController.java
+++ FoodForThoughtSpring/src/main/java/com/revature/controllers/PictureController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -66,7 +67,7 @@ public class PictureController {
 	}
 
 	@PostMapping(path = "/download")
-	public ResponseEntity<List<Picture>> getById(@RequestParam("username") String username) {
+	public ResponseEntity<List<Picture>> getById(@RequestBody String username) {
 		
 //		User u = login.findUser(username);
 //		Optional<Picture> pic = ps.findByUser(username);
