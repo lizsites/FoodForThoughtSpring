9e4ba7160484f4fe95ae1f5a4210961b9c99554a
lizsites <69205363+lizsites@users.noreply.github.com>
2020-09-24 17:04:23 -0500

    adding tests again

--- FoodForThoughtSpring/src/test/java/com/revature/FoodForThoughtSpringApplicationTests.java
+++ FoodForThoughtSpring/src/test/java/com/revature/FoodForThoughtSpringApplicationTests.java
@@ -11,7 +11,7 @@
 //class FoodForThoughtSpringApplicationTests extends Runner {
 //
 //	public static void main(String[] args) {
-//		SpringApplication.run(FoodForThoughtSpringApplication.class, args);
+//		
 //		
 //	}
 //
---
+++ FoodForThoughtSpring/src/test/java/com/revature/SpringRunner.java
@@ -0,0 +1,30 @@
+package com.revature;
+
+import org.junit.runner.Description;
+import org.junit.runner.Runner;
+import org.junit.runner.notification.RunNotifier;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SpringRunner extends Runner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FoodForThoughtSpringApplication.class, args);
+
+	}
+
+	@Override
+	public Description getDescription() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void run(RunNotifier arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
